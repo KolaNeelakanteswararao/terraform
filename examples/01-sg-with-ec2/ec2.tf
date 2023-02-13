f2372fcae2f5343f25c7e0ebbb357274f73c7bd6
neela <neelakanta165@gmail.com>
2023-02-13 16:54:19 +0530

    updating .

--- examples/01-sg-with-ec2/ec2.tf
+++ examples/01-sg-with-ec2/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0f9d21984aa37d90a"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-
   tags = {
     Name = "web"
   }
