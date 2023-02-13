9ce013deb61ab04b26ff1e516a09eb1458f25435
neela <neelakanta165@gmail.com>
2023-02-13 21:37:23 +0530

    updating .

--- examples/01-sg-with-ec2/provider.tf
+++ examples/01-sg-with-ec2/provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "us-east-1"
-  endpoints {
-    sts = "https://sts.us-east-1.amazonaws.com"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b62"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
   }
 }
