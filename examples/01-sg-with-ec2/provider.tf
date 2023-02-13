2654698d87506b0c610e031f4f9a4c2b02fd2685
neela <neelakanta165@gmail.com>
2023-02-13 16:47:12 +0530

    updating .

--- examples/01-sg-with-ec2/provider.tf
+++ examples/01-sg-with-ec2/provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAX3CIVJVDBSLS7K3B"
+  secret_key = "STqPp8kmIw4aomVHbTFNyes7lYTjyjWSKYYJFKF0"
+  endpoints {
+    sts = "https://sts.us-east-1.amazonaws.com"
+  }
 }
